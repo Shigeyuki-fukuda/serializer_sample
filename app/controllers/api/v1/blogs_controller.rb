2ce722f7498aa4f1286c4412b9a654fb97971fd6
Shigeyuki-fukuda <urbansonnet1990@gmail.com>
2020-12-05 21:22:51 +0900

    feat: Blogコントローラーを仮実装

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -1,7 +1,29 @@
 class Api::V1::BlogsController < ApplicationController
   def index
+    @blogs = Blog.order(created_at: :desc)
+    render json: {
+      blogs: blogs.map do |blog|
+        {
+          id: blog.id,
+          title: blog.titile,
+          content: blog.content
+        }
+      end
+    }
   end
 
   def create
+    blog = Blog.new(blog_params)
+    if blog.save
+      head: :success
+    else
+      head: :bad_request
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end
