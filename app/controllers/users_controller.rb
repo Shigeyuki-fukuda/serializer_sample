0b9dddb9e7463d3e9ace4416fec4f77b5d822ac0
Shigeyuki-fukuda <urbansonnet1990@gmail.com>
2020-12-06 21:00:44 +0900

    feat: Usersコントローラーを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,23 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      head :ok
+    else
+      head :bad_request
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirm)
   end
 end
